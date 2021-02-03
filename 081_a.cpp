a10d83c69db42d8ce68a1834c299885ffaa3abff
Tomo-bit <tomo001007@gmail.com>
2021-02-03 23:44:06 +0900

    base

---
+++ 081_a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string a;
+    cin >> a;
+
+   int f ;
+   f =0;
+   for (int i = 0; i<3; i++){
+       if(a[i] == '1'){
+           f += 1;
+       }
+   }
+   cout << f << endl;
+}
---
+++ 172_a.py
@@ -0,0 +1,3 @@
+a = input()
+a = int(a)
+print(a+ a**2 + a**3 )
