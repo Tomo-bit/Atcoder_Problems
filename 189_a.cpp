08aa59a60a053e88b70cfc13d60fa04aca98c5d6
Tomo-bit <tomo001007@gmail.com>
2021-02-03 23:23:07 +0900

    'base'

---
+++ 189_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	 string abc;
+  cin >> abc;
+  if (abc[0] == abc[1] && abc[1] == abc[2]){
+  	cout << "Won" << endl;
+  }
+  else {
+  	cout << "Lost" << endl;}
+}
