2205927786be988f367cc449b2365b3edcf58a57
Tomo-bit <tomo001007@gmail.com>
2021-02-04 04:06:12 +0900

    base

--- 190_a.cpp
+++ 190_a.cpp
@@ -1,20 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-	int a,b,c;
- cin >> a >> b >> c;
-  if (c==0){
-  	if (a<=b){
-   		cout << "Aoki" << endl;}
-    if (a>b){
-    	cout << "Takahashi" << endl ;}
-  }
-  if (c==1){
-  	if (a>=b){
-    	cout << "Takahashi" << endl;}
-    if (a<b){
-    	cout << "Aoki" << endl;}
-  }
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (c == 0)
+	{
+		if (a <= b)
+		{
+			cout << "Aoki" << endl;
+		}
+		if (a > b)
+		{
+			cout << "Takahashi" << endl;
+		}
+	}
+	if (c == 1)
+	{
+		if (a >= b)
+		{
+			cout << "Takahashi" << endl;
+		}
+		if (a < b)
+		{
+			cout << "Aoki" << endl;
+		}
+	}
 }
-
